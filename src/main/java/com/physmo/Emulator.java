5a596ffc71f3281049262319f823d75a7f0c6d24
Nick Donnelly <nickd3000@hotmail.com>
2024-04-24 23:34:52 +0100

    Added some more games

--- src/main/java/com/physmo/Emulator.java
+++ src/main/java/com/physmo/Emulator.java
@@ -18,20 +18,47 @@ public class Emulator {
         nesCart.mapper.attachRig(rig, nesCart);
         rig.attachCart(nesCart);
 
-        rig.runForCycles(2000000);
+        rig.runForCycles(200000000);
 
     }
 
     public static String getGame() {
+        // Mapper 000
 //        return "mario bros.nes";
 //        return "super mario bros.nes";
 //        return "donkey kong.nes";
 //        return "balloon fight.nes";
 //        return "pac-man.nes";
-        return "bomberman.nes";
+//        return "bomberman.nes";
 //        return "golf.nes";
 //        return "ice climber.nes";
-//        return "ice hockey.nes";
+        return "ice hockey.nes";
+
+        // Mapper 001
+//        return "tetris.nes";
+
+        // Mapper 002
+//        return "Castlevania.nes";
+//        return "RainbowIslands.nes";
+//        return "SuperPitfall.nes";
+//        return  "Ikari Warriors.nes";
+//        return "Life Force.nes";
+//        return "TotalRecall.nes"; // 002
+
+
+        // Mapper 003
+//        return "arkanoid.nes";
+//        return "gradius.nes";
+//        return "q*bert.nes";
+//        return "BumpNJump.nes";
+//        return "Castlequest.nes";
+//        return "MightyBombJack.nes";
+//            return "SolomonsKey.nes";
+
+//        return "BuraiFighter.nes"; // 004
+//        return "MysteryQuest.nes"; // 64
+
+//        return "MegaMan.nes"; // 066
     }
 
 }
